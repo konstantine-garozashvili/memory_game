27b81af995a62e0c2646c3150947b651b44fd9f2
konstantine-garozashvili <konstantine.garozashvili@laplateforme.io>
2024-09-02 13:02:07 +0200

    dd

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,8 @@
 
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $servername = "172.20.10.2";  // Adresse IP de votre serveur MariaDB
 $username = "linux-server";   // Nom d'utilisateur MariaDB
 $password = "010203";         // Mot de passe MariaDB
@@ -13,6 +16,11 @@ if ($conn->connect_error) {
     die("Échec de la connexion : " . $conn->connect_error);
 }
 echo "Connexion réussie à la base de données memory_game";
+
+// Error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 
 // Fermer la connexion
 
