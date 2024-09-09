57f0431495d59f05eb72fbc580ed639cf5c18513
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-09-09 14:48:31 +0200

    update

--- sql/memory_game_updated.sql
+++ sql/memory_game_updated.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Sep 09, 2024 at 12:43 PM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `memory_game`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `chat_messages`
---
 
 CREATE TABLE `chat_messages` (
   `id` int NOT NULL,
@@ -34,19 +6,8 @@ CREATE TABLE `chat_messages` (
   `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `chat_messages`
---
 
-INSERT INTO `chat_messages` (`id`, `user_id`, `message`, `timestamp`) VALUES
-(8, 17, 'salut', '2024-09-09 11:56:10'),
-(9, 16, 'cava?', '2024-09-09 11:56:14');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `games`
---
 
 CREATE TABLE `games` (
   `id` int NOT NULL,
@@ -61,12 +22,6 @@ CREATE TABLE `games` (
   `game_mode` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'hidden_memory'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `game_moves`
---
-
 CREATE TABLE `game_moves` (
   `id` int NOT NULL,
   `game_id` int NOT NULL,
@@ -76,11 +31,7 @@ CREATE TABLE `game_moves` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `invitations`
---
 
 CREATE TABLE `invitations` (
   `id` int NOT NULL,
@@ -89,12 +40,6 @@ CREATE TABLE `invitations` (
   `status` enum('pending','accepted','declined') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'pending',
   `game_mode` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
@@ -106,107 +51,57 @@ CREATE TABLE `users` (
   `profile_picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'uploads/default_profile.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `last_login`, `profile_picture`) VALUES
-(16, 'abdelaa', '$2y$10$TX1jKbr84qxyJYlg3TNFhumJ5gk.C0HGkryBQ9ZJX8EXcCNdNR8u.', 'abdela@gmail.com', 'user', '2024-09-09 12:08:00', NULL),
-(18, 'konstantine', '$2y$10$0w1IfMnEALTa1Fl/joWhNejfMJDHkyeF7wd8OId2KyyOp0oXLVzma', 'garozashvili25@gmail.com', 'user', '2024-09-09 12:42:21', 'uploads/Screenshot 2024-08-22 131722.png');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `chat_messages`
---
 ALTER TABLE `chat_messages`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `games`
---
+
 ALTER TABLE `games`
   ADD PRIMARY KEY (`id`),
   ADD KEY `player1_id` (`player1_id`),
   ADD KEY `player2_id` (`player2_id`);
 
---
--- Indexes for table `game_moves`
---
+
 ALTER TABLE `game_moves`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `invitations`
---
+
 ALTER TABLE `invitations`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sender_id` (`sender_id`),
   ADD KEY `receiver_id` (`receiver_id`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `chat_messages`
---
 ALTER TABLE `chat_messages`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- AUTO_INCREMENT for table `games`
---
+
 ALTER TABLE `games`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
---
--- AUTO_INCREMENT for table `game_moves`
---
+
 ALTER TABLE `game_moves`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1472;
 
---
--- AUTO_INCREMENT for table `invitations`
---
+
 ALTER TABLE `invitations`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `games`
---
 ALTER TABLE `games`
   ADD CONSTRAINT `games_ibfk_1` FOREIGN KEY (`player1_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `games_ibfk_2` FOREIGN KEY (`player2_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 
---
--- Constraints for table `invitations`
---
 ALTER TABLE `invitations`
   ADD CONSTRAINT `invitations_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `invitations_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
